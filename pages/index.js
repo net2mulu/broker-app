b130155760917b845315505d4459ad15238aa6e5
net <nati2mulugeta@gmail.com>
2022-09-18 23:20:24 +0300

    final

--- pages/index.js
+++ pages/index.js
@@ -19,16 +19,16 @@ export default function Home() {
         <nav className="navbar navbar-expand-lg">
           <div className="container">
             <a className="navbar-brand" href="index.html">
-              <i className="fa-light fa-signature"></i>              SEMERE KIDANE
+              <i className="fa-light fa-signature"></i>
+              SEMERE KIDANE
             </a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon" />
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav ml-auto">
-
                 <li className="nav-item">
-                  <a href="contact.html" className="nav-link contact">Contact</a>
+                  <a href="#footer" className="nav-link contact">Contact</a>
                 </li>
               </ul>
             </div>
@@ -40,9 +40,9 @@ export default function Home() {
             <div className="row">
               <div className="col-lg-6 col-md-10 col-12 d-flex flex-column justify-content-center align-items-center">
                 <div className="hero-text">
-                  <h1 className="text-white" data-aos="fade-up">We are ready for your digital marketing</h1>
-                  <a href="contact.html" className="custom-btn btn-bg btn mt-3" data-aos="fade-up" data-aos-delay={100}>Let us discuss together!</a>
-                  <strong className="d-block py-3 pl-5 text-white" data-aos="fade-up" data-aos-delay={200}><i className="fa fa-phone mr-2" /> +99 080 070 4224</strong>
+                  <h1 className="text-white" data-aos="fade-up">Everything you need to know about the assets to rent</h1>
+                  <a href="contact.html" className="custom-btn btn-bg btn mt-3" data-aos="fade-up" data-aos-delay={100}>Give us a Call any time</a>
+                  <strong className="d-block py-3 pl-5 text-white" data-aos="fade-up" data-aos-delay={200}><i className="fa fa-phone mr-2" /> 07117492 | 07633438</strong>
                 </div>
               </div>
               <div className="col-lg-6 col-12">
@@ -59,9 +59,8 @@ export default function Home() {
             <div className="row">
               <div className="col-lg-7 mx-auto col-md-10 col-12">
                 <div className="about-info">
-                  <h2 className="mb-4" data-aos="fade-up">the best <strong>Digital Marketing agency</strong> in Rio de Janeiro</h2>
-                  <p className="mb-0" data-aos="fade-up">Total 5 HTML pages are included in this template from TemplateMo website. Please check 2 <a href="blog.html">blog</a> pages, <a href="project-detail.html">project</a> page, and <a href="contact.html">contact</a> page.
-                    <br /><br />You are <strong>allowed</strong> to use this template for commercial or non-commercial purpose. You are NOT allowed to redistribute the template ZIP file on template collection websites.</p>
+                  <h2 className="mb-4" data-aos="fade-up">We are the best <strong>dealers 7(broker)</strong> in ertirea with valuable connections</h2>
+                  <p className="mb-0" data-aos="fade-up">Make sure to contact us before you decide on renting,buying a place like (villas, appartments, comercial shops) in ertirea.</p>
                 </div>
                 <div className="about-image" data-aos="fade-up" data-aos-delay={200}>
                   <img src="images/office.png" className="img-fluid" alt="office" />
@@ -76,66 +75,56 @@ export default function Home() {
             <div className="row">
               <div className="col-lg-12 col-12">
                 <h2 className="mb-5 text-center" data-aos="fade-up">
-                  Please take a look through our
-                  <strong>featured Digital Trends</strong>
+                  Please take a look at<br></br>
+                  <strong>What we offer</strong>
                 </h2>
                 <div className="owl-carousel owl-theme" id="project-slide">
                   <div className="item project-wrapper" data-aos="fade-up" data-aos-delay={100}>
-                    <img src="images/project/project-image01.jpg" className="img-fluid" alt="project image" />
+                    <img src="http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQ2Zakzmja4CiOhjiMkortp22AzAO_VYAn2fMM7uqYoMzAcuvvDuwB_VcDI2vqFP7KI" className="img-fluid" alt="project image" />
                     <div className="project-info">
-                      <small>Marketing</small>
                       <h3>
                         <a href="project-detail.html">
-                          <span>Sweet Go Agency</span>
-                          <i className="fa fa-angle-right project-icon" />
+                          <span>Commercial Shops</span>
                         </a>
                       </h3>
                     </div>
                   </div>
                   <div className="item project-wrapper" data-aos="fade-up">
-                    <img src="images/project/project-image02.jpg" className="img-fluid" alt="project image" />
+                    <img src="https://i.pinimg.com/originals/1e/29/26/1e292677d2a1c2feb0ea612f48356713.jpg" className="img-fluid" alt="project image" />
                     <div className="project-info">
-                      <small>Website</small>
                       <h3>
                         <a href="project-detail.html">
-                          <span>Smart Ladies</span>
-                          <i className="fa fa-angle-right project-icon" />
+                          <span>Guest houses</span>
                         </a>
                       </h3>
                     </div>
                   </div>
                   <div className="item project-wrapper" data-aos="fade-up">
-                    <img src="images/project/project-image03.jpg" className="img-fluid" alt="project image" />
+                    <img src="https://www.ribapix.com/images/thumbs/036/0366942_RIBA4474_600.jpeg" className="img-fluid" alt="project image" />
                     <div className="project-info">
-                      <small>Branding</small>
                       <h3>
                         <a href="project-detail.html">
-                          <span>Shoes factory</span>
-                          <i className="fa fa-angle-right project-icon" />
+                          <span>Appartments</span>
                         </a>
                       </h3>
                     </div>
                   </div>
                   <div className="item project-wrapper" data-aos="fade-up">
-                    <img src="images/project/project-image04.jpg" className="img-fluid" alt="project image" />
+                    <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/56379526.jpg?k=cc74113269f666ce89437125d0bc4e3ae72ef2d9e582e46d0ef2e1d82610feaa&o=&hp=1" className="img-fluid" alt="project image" />
                     <div className="project-info">
-                      <small>Social Media</small>
                       <h3>
                         <a href="project-detail.html">
-                          <span>Race Bicycle</span>
-                          <i className="fa fa-angle-right project-icon" />
+                          <span>Hotels</span>
                         </a>
                       </h3>
                     </div>
                   </div>
                   <div className="item project-wrapper" data-aos="fade-up">
-                    <img src="images/project/project-image05.jpg" className="img-fluid" alt="project image" />
+                    <img src="https://518192-1674976-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2017/11/dsc_1150-dsc_1154-765x510.jpg" className="img-fluid" alt="project image" />
                     <div className="project-info">
-                      <small>Video</small>
                       <h3>
                         <a href="project-detail.html">
-                          <span>Ultimate HealthCare</span>
-                          <i className="fa fa-angle-right project-icon" />
+                          <span>Vilas</span>
                         </a>
                       </h3>
                     </div>
@@ -145,39 +134,18 @@ export default function Home() {
             </div>
           </div>
         </section>
-        {/* TESTIMONIAL */}
-        <section className="testimonial section-padding">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-6 col-md-5 col-12">
-                <div className="contact-image" data-aos="fade-up">
-                  <img src="images/female-avatar.png" className="img-fluid" alt="website" />
-                </div>
-              </div>
-              <div className="col-lg-6 col-md-7 col-12">
-                <h4 className="my-5 pt-3" data-aos="fade-up" data-aos-delay={100}>Client Testimonials</h4>
-                <div className="quote" data-aos="fade-up" data-aos-delay={200} />
-                <h2 className="mb-4" data-aos="fade-up" data-aos-delay={300}>Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo.</h2>
-                <p data-aos="fade-up" data-aos-delay={400}>
-                  <strong>Mary Zoe</strong>
-                  <span className="mx-1">/</span>
-                  <small>Digital Agency (CEO)</small>
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-        <footer className="site-footer">
+
+        <footer id="footer" className="site-footer">
           <div className="container">
             <div className="row">
               <div className="col-lg-5 mx-lg-auto col-md-8 col-10">
-                <h1 className="text-white" data-aos="fade-up" data-aos-delay={100}>We make creative <strong>brands</strong> only.</h1>
+                <h1 className="text-white" data-aos="fade-up" data-aos-delay={100}>We Offer <strong>Assured</strong> Dealership.</h1>
               </div>
               <div className="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay={200}>
-                <h4 className="my-4">Contact Info</h4>
+                <h4 className="my-4">whatsapp </h4>
                 <p className="mb-1">
                   <i className="fa fa-phone mr-2 footer-icon" />
-                  +99 080 070 4224
+                  002917117492
                 </p>
                 <p>
                   <a href="#">
@@ -187,30 +155,30 @@ export default function Home() {
                 </p>
               </div>
               <div className="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay={300}>
-                <h4 className="my-4">Our Studio</h4>
+                <h4 className="my-4">Location</h4>
                 <p className="mb-1">
                   <i className="fa fa-home mr-2 footer-icon" />
-                  Av. Lúcio Costa - Barra da Tijuca, Rio de Janeiro - RJ, Brazil
+                  Eritirea, Asmara
                 </p>
               </div>
               <div className="col-lg-4 mx-lg-auto text-center col-md-8 col-12" data-aos="fade-up" data-aos-delay={400}>
-                <p className="copyright-text">Copyright © 2020 Your Company
+                <p className="copyright-text">Copyright © 2020
                   <br />
-                  <a rel="nofollow noopener" href="https://templatemo.com">Design: TemplateMo</a></p>
+                  <a rel="nofollow noopener" href="#">semerekidane.com</a></p>
               </div>
               <div className="col-lg-4 mx-lg-auto col-md-6 col-12" data-aos="fade-up" data-aos-delay={500}>
                 <ul className="footer-link">
                   <li><a href="#">Stories</a></li>
                   <li><a href="#">Work with us</a></li>
-                  <li><a href="#">Privacy</a></li>
+                  <li><a href="#">wide offers</a></li>
                 </ul>
               </div>
               <div className="col-lg-3 mx-lg-auto col-md-6 col-12" data-aos="fade-up" data-aos-delay={600}>
                 <ul className="social-icon">
                   <li><a href="#" className="fa fa-instagram" /></li>
-                  <li><a href="#" className="fa fa-twitter" /></li>
-                  <li><a href="#" className="fa fa-dribbble" /></li>
-                  <li><a href="#" className="fa fa-behance" /></li>
+                  {/* <li><a href="#" className="fa fa-twitter" /></li> */}
+                  <li><a href="#" className="fa fa-whatsapp" /></li>
+                  <li><a href="#" className="fa fa-facebook" /></li>
                 </ul>
               </div>
             </div>
@@ -221,7 +189,7 @@ export default function Home() {
       <Script src="js/bootstrap.min.js" />
       <Script src="js/aos.js" />
       <Script src="js/owl.carousel.min.js" />
-      <Script src="js/smoothscroll.js" />
+      <Script src="js/smoothscroll.js" strategy="lazylazyOnload" />
       <Script src="js/custom.js" />
     </div>
   )
