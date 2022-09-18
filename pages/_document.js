bee650ddb4d07775cef36feea1f32109c890612c
net <nati2mulugeta@gmail.com>
2022-09-18 22:03:37 +0300

    next commit

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+
+
+                <link rel="stylesheet" href="css/bootstrap.min.css" />
+                <link rel="stylesheet" href="css/font-awesome.min.css" />
+                <link rel="stylesheet" href="css/aos.css" />
+                <link rel="stylesheet" href="css/owl.carousel.min.css" />
+                <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+
+                <link rel="stylesheet" href="css/templatemo-digital-trend.css" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+
+            </body>
+        </Html>
+    )
+}
